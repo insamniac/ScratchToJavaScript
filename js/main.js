c5cd42292e47cfc69da5457d331816adf145af69
Sam Burke <burkesamuel@gmail.com>
2018-02-01 18:07:16 -0800

    add a canvas and a few basics

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+
+(function() {
+    var canvas = document.getElementById('canvas'),
+            context = canvas.getContext('2d');
+
+    // resize the canvas to fill browser window dynamically
+    window.addEventListener('resize', resizeCanvas, false);
+
+    function resizeCanvas() {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+            doGameFrame();
+    }
+    resizeCanvas();
+
+})();
+
+
