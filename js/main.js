83ba727ed0b9cb6e287581fd01e4184bcb58cd53
Sam Burke <burkesamuel@gmail.com>
2018-02-04 16:36:05 -0800

    i forget

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-
-
 (function() {
+
+
     var canvas = document.getElementById('canvas'),
             context = canvas.getContext('2d');
+	var backgroundImage = new Image();
+backgroundImage.src = 'images/background.png';
+
 
     // resize the canvas to fill browser window dynamically
     window.addEventListener('resize', resizeCanvas, false);
@@ -10,9 +13,19 @@
     function resizeCanvas() {
             canvas.width = window.innerWidth;
             canvas.height = window.innerHeight;
-            doGameFrame();
     }
     resizeCanvas();
+
+
+var  refreshRate = 1000 / 60;
+
+function step(timestamp) {
+  context.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height); 
+  window.requestAnimationFrame(step);
+}
+
+window.requestAnimationFrame(step);
+
 
 })();
 
