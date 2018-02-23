9196f991fdaebbadaa99c6fa4b90bc98fc4402c8
SavvyDB <savvy.d.burke@gmail.com>
2018-02-22 18:59:33 -0800

    what

--- js/game.js
+++ js/game.js
@@ -76,9 +76,17 @@ function initialize() {
 }
 
 function pointToward(obj1, obj2, precision) {
-    var d =getDistance(obj1, obj2);
-    obj1.dir.x = (obj2.pos.x - obj1.pos.x) / d;
-    obj1.dir.y = (obj2.pos.y - obj1.pos.y) / d; 
+    precision = precision || 1;
+    if (obj1.pos.x > obj2.pos.x) {
+        obj1.dir.x = -1;
+    } else {
+        obj1.dir.x = 1;
+    }
+    if (obj1.pos.y > obj2.pos.y) {
+        obj1.dir.y = -1;
+    } else {
+        obj1.dir.y = 1;
+    }
 }
 
 function move(obj) {
@@ -95,16 +103,16 @@ function move(obj) {
 }
 
 function doMovement() {
-    [player].concat(entities).forEach(function(obj) {
-        if (obj.dir && obj.dir.x > 0) {
-            obj.facing = 'right';
-        } else if (obj.dir && obj.dir.x < 0) {
-            obj.facing = 'left';
-        }
+    if (player.dir.x > 0) {
+        player.costume = player.img.right;
+    } else if (player.dir.x < 0) {
+        player.costume = player.img.left;
+    }
+
+    move(player);
+    entities.forEach(function(obj) {
         if (obj.type == 'enemy') {
-            if (getDistance(obj, player) < (obj.attackRange || 800)) {
-                pointToward(obj, player);
-            }
+            pointToward(obj, player);
         }
         move(obj);
     });
@@ -121,7 +129,6 @@ function checkCollisions() {
                     if (areTouching(obj1, obj2) && !obj1.destroy && !obj2.destroy) {
                         obj1.destroy = true;
                         obj2.destroy = true;
-                        explode(obj1);
                         explode(obj2);
                         if (obj1.type == 'enemy') {
                             sounds.play(obj1.sound);
@@ -160,6 +167,7 @@ function checkCollisions() {
             obj.destroy = true;
             loseALife();
         } else if (obj.type == 'powerup' && areTouching(obj, player))  {
+            sounds.play('yay');
             BULLET_SIZE+=1
             player.bullets+=1;
             player.size+=1;
@@ -223,49 +231,45 @@ function destroyAndCreate(timestamp) {
         console.log('New Life Created at: ' + tick + '.  Next At: ' + nextLife);
     }
 }
-
-function levelUp() {
-    player.level+=1;
-    BG_SOURCE.stop();
-    sounds.play('level-up');
-    cycleBackgroundMusic();
-}
-
 function checkLevelUp() {
   if (player.score == 15 && player.level == 1) {
-    levelUp();
-    BG_TINT='blue';
+    player.level = 2;
     ENEMY_DELAY -= 1;
   }
-  if (player.score == 40 && player.level == 2) {
-    levelUp();
-    BG_TINT='green';
+  if (player.score == 30 && player.level == 2) {
+    player.level = 3;
     player.size += 50;
     BULLET_SPEED += 20;
     ENEMY_DELAY -= 1;
   }
-  if (player.score == 60 && player.level == 3) {
-      levelUp();
-    BG_TINT='gray';
+  if (player.score == 50 && player.level == 3) {
+    player.level = 4;
     BULLET_SPEED += 50;
     player.bullets += 20;
     ENEMY_DELAY -= 1;
   }
   if (player.score == 100 && player.level == 4) {
-      levelUp();
-    BG_TINT='red';
+    player.level = 5;
     BULLET_SPEED += 60;
     player.bullets += 30;
     ENEMY_DELAY -= 1;
   }
+  if (player.score == 300 && player.level == 5) {
+    player.level = 6;
+    player.bullets += 70;
+    ENEMY_DELAY -= 1;
+  }
+  if (player.score == 500 && player.level == 6) {
+    player.level = 7;
+    BULLET_SPEED += 60;
+    player.bullets += 150;
+    ENEMY_DELAY -= 2;
+  }
 
 }
 
-var ANIMATION_FRAME=0;
-
 function gameStep(timestamp) {
     if (!PAUSED && !GAMEOVER) {
-        ANIMATION_FRAME=~~(timestamp / 500)  % 2;
         doMovement();
         checkCollisions();
         destroyAndCreate(timestamp);
@@ -316,16 +320,3 @@ function gameover() {
 
 initialize();
 window.requestAnimationFrame(gameStep);
-
-                                                                        
-document.getElementById('pause-control').addEventListener('click', function(e) {
-//    console.log(e);
-    if (e.target.classList.contains('paused')) {
-        PAUSED=false;
-        e.target.classList.remove("paused");
-    } else {
-        PAUSED=true;
-        e.target.classList.add("paused");
-    }
-    document.activeElement.blur();
-});
